ef8aa1f7d5d7e3484197cf911fd0d675130bd9a6
unknown <nitinai524@gmail.com>
2017-11-20 23:25:45 +0700

    testMessesging

--- index.php
+++ index.php
@@ -8,24 +8,24 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token ='npw3T/uhYY7LP4WWi/ZGcecDTuvBB4uvU1nno/xUKyv5goSwafRNSzFX7DWTMAzxerjSZ+gIoigCY3TIpAfmXydCORXGct3Jm/EUQq+sOtYfQy+qqlwXx65DFfAUCHuhUjCq+Q4H6fJvdrdeTI89nQdB04t89/1O/w1cDnyilFU=';
 $channel_secret = '09601a72fa04f02b55b4587b9f7347c4';
 // Get message from Line API
+123
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 // Loop through each event
 foreach ($events['events'] as $event) {
-// Line API send a lot of event type, we interested in message only.
-if ($event['type'] == 'message') {
 // Get replyToken
 $replyToken = $event['replyToken'];
-switch($event['message']['type']) {
-case 'sticker':
-$messageID = $event['message']['packageId'];
-// Reply message
-$respMessage = 'Hello, your Sticker Package ID is '. $messageID;
+$ask = $event['message']['text'];
+switch(strtolower($ask)) {
+case 'm':
+$respMessage = 'What sup man. Go away!';
+break;
+case 'f':
+$respMessage = 'Love you lady.';
 break;
 default:
-// Reply message
-$respMessage = 'Please send Sticker only';
+$respMessage = 'What is your sex? M or F';
 break;
 }
 $httpClient = new CurlHTTPClient($channel_token);
@@ -34,6 +34,6 @@ $textMessageBuilder = new TextMessageBuilder($respMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
 }
 }
-}
 echo "OK";
+
 ?>
