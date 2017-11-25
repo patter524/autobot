65d279d44d90f05a9d5f601ff037c1432f22c2bf
patter524 <nitinai@gmail.com>
2017-11-25 22:32:42 +0700

    save daily

--- index.php
+++ index.php
@@ -12,25 +12,28 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 // Loop through each event
 foreach ($events['events'] as $event) {
-// Line API send a lot of event type, we interested in message only.
-if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+    // Line API send a lot of event type, we interested in message only.
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
     // Get replyToken
     $replyToken = $event['replyToken'];
-    switch($event['message']['text']) {
-    case 'tel':
-    $respMessage = '089-5124512';
-    break;
-    case 'address':
-    $respMessage = '99/451 Muang Nonthaburi';
-    break;
-    case 'boss':
-    $respMessage = '089-2541545';
-    break;
-    case 'idcard':
-    $respMessage = '5845122451245';
-    break;
-    default:
-    break;
+    // Split message then keep it in database.
+    $appointments = explode(',', $event['message']['text']);
+    if(count($appointments) == 2) {
+    $host = 'ec2-23-23-249-169.compute-1.amazonaws.com';
+    $dbname = 'd355nho8tfr4eo';
+    $user = 'fmltqorfsipopg';
+    $pass = '39f71bc0396e4a5515a9e6215f1fe57ae6c118b1a58695b2e744cf9d91ba5dd1';
+    $connection = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+    $params = array(
+    'time' => $appointments[0],
+    'content' => $appointments[1],
+    );
+    $statement = $connection->prepare("INSERT INTO appointments (time, content) VALUES (:time,
+    :content)");
+    $result = $statement->execute($params);
+    $respMessage = 'Your appointment has saved.';
+    }else{
+    $respMessage = 'You can send appointment like this "12.00,House keeping." ';
     }
     $httpClient = new CurlHTTPClient($channel_token);
     $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
