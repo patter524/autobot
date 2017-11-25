88ed80208a9f4f8f070d3d007091daa25635926d
patter524 <nitinai@gmail.com>
2017-11-25 22:19:31 +0700

    TestSystem

--- index.php
+++ index.php
@@ -12,24 +12,32 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 // Loop through each event
 foreach ($events['events'] as $event) {
-// Get replyToken
-$replyToken = $event['replyToken'];
-$ask = $event['message']['text'];
-switch(strtolower($ask)) {
-case 'm':
-$respMessage = 'What sup man. Go away!';
-break;
-case 'f':
-$respMessage = 'Love you lady.';
-break;
-default:
-$respMessage = 'What is your sex? M1 or F1aa';
-break;
-}
-$httpClient = new CurlHTTPClient($channel_token);
-$bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-$textMessageBuilder = new TextMessageBuilder($respMessage);
-$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-}
-}
-echo "OK";
+// Line API send a lot of event type, we interested in message only.
+if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+    // Get replyToken
+    $replyToken = $event['replyToken'];
+    switch($event['message']['text']) {
+    case 'tel':
+    $respMessage = '089-5124512';
+    break;
+    case 'address':
+    $respMessage = '99/451 Muang Nonthaburi';
+    break;
+    case 'boss':
+    $respMessage = '089-2541545';
+    break;
+    case 'idcard':
+    $respMessage = '5845122451245';
+    break;
+    default:
+    break;
+    }
+    $httpClient = new CurlHTTPClient($channel_token);
+    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
+    $textMessageBuilder = new TextMessageBuilder($respMessage);
+    $response = $bot->replyMessage($replyToken, $textMessageBuilder);
+    }
+    }
+    }
+    echo "OK";
+    
